7396c7bae4dffa513c3f26f5b38dabea60ed53c4
StatelyCloud <Release Bot infra@stately.cloud>
2024-11-20 00:43:17 +0000

    Update from https://github.com/StatelyCloud/stately/commit/6a97d9fa92c71e65a6756aec68217f8e2896c8da

--- statelydb/lib/api/db/list_token_pb2.py
+++ statelydb/lib/api/db/list_token_pb2.py
@@ -24,7 +24,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x13\x64\x62/list_token.proto\x12\nstately.db\"h\n\tListToken\x12\x1d\n\ntoken_data\x18\x01 \x01(\x0cR\ttokenData\x12!\n\x0c\x63\x61n_continue\x18\x02 \x01(\x08R\x0b\x63\x61nContinue\x12\x19\n\x08\x63\x61n_sync\x18\x03 \x01(\x08R\x07\x63\x61nSyncBi\n\x0e\x63om.stately.dbB\x0eListTokenProtoP\x01\xa2\x02\x03SDX\xaa\x02\nStately.Db\xca\x02\nStately\\Db\xe2\x02\x16Stately\\Db\\GPBMetadata\xea\x02\x0bStately::Dbb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x13\x64\x62/list_token.proto\x12\nstately.db\"\x94\x01\n\tListToken\x12\x1d\n\ntoken_data\x18\x01 \x01(\x0cR\ttokenData\x12!\n\x0c\x63\x61n_continue\x18\x02 \x01(\x08R\x0b\x63\x61nContinue\x12\x19\n\x08\x63\x61n_sync\x18\x03 \x01(\x08R\x07\x63\x61nSync\x12*\n\x11schema_version_id\x18\x04 \x01(\rR\x0fschemaVersionIdBi\n\x0e\x63om.stately.dbB\x0eListTokenProtoP\x01\xa2\x02\x03SDX\xaa\x02\nStately.Db\xca\x02\nStately\\Db\xe2\x02\x16Stately\\Db\\GPBMetadata\xea\x02\x0bStately::Dbb\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -32,6 +32,6 @@ _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'db.list_token_pb2', _global
 if not _descriptor._USE_C_DESCRIPTORS:
   _globals['DESCRIPTOR']._loaded_options = None
   _globals['DESCRIPTOR']._serialized_options = b'\n\016com.stately.dbB\016ListTokenProtoP\001\242\002\003SDX\252\002\nStately.Db\312\002\nStately\\Db\342\002\026Stately\\Db\\GPBMetadata\352\002\013Stately::Db'
-  _globals['_LISTTOKEN']._serialized_start=35
-  _globals['_LISTTOKEN']._serialized_end=139
+  _globals['_LISTTOKEN']._serialized_start=36
+  _globals['_LISTTOKEN']._serialized_end=184
 # @@protoc_insertion_point(module_scope)
--- statelydb/lib/api/db/list_token_pb2.pyi
+++ statelydb/lib/api/db/list_token_pb2.pyi
@@ -5,11 +5,13 @@ from typing import ClassVar as _ClassVar, Optional as _Optional
 DESCRIPTOR: _descriptor.FileDescriptor
 
 class ListToken(_message.Message):
-    __slots__ = ("token_data", "can_continue", "can_sync")
+    __slots__ = ("token_data", "can_continue", "can_sync", "schema_version_id")
     TOKEN_DATA_FIELD_NUMBER: _ClassVar[int]
     CAN_CONTINUE_FIELD_NUMBER: _ClassVar[int]
     CAN_SYNC_FIELD_NUMBER: _ClassVar[int]
+    SCHEMA_VERSION_ID_FIELD_NUMBER: _ClassVar[int]
     token_data: bytes
     can_continue: bool
     can_sync: bool
-    def __init__(self, token_data: _Optional[bytes] = ..., can_continue: bool = ..., can_sync: bool = ...) -> None: ...
+    schema_version_id: int
+    def __init__(self, token_data: _Optional[bytes] = ..., can_continue: bool = ..., can_sync: bool = ..., schema_version_id: _Optional[int] = ...) -> None: ...
